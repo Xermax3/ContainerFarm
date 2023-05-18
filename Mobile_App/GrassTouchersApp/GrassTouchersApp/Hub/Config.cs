95ec4a4099015a83d78542f25a8245c0b6221661
Maxence Roy <60619324+Xermax3@users.noreply.github.com>
2023-05-18 03:47:03 -0400

    Replaced invalid keys with placeholders

--- Mobile_App/GrassTouchersApp/GrassTouchersApp/Hub/Config.cs
+++ Mobile_App/GrassTouchersApp/GrassTouchersApp/Hub/Config.cs
@@ -10,11 +10,11 @@ namespace GrassTouchersApp.Hub
     /// <summary> IoTHub related values and keys. </summary>
     public static class Config
     {
-        public static string EventHubConnectionString = "Endpoint=sb://ihsuprodblres094dednamespace.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=FHi9mLIpTlAZC3im336ASe43wO7e4Ydx+WrY4BcLe2k=;EntityPath=iothub-ehub-420-6p3-18203029-88a4b3216e";
-        public static string EventHubName = "iothub-ehub-420-6p3-18203029-88a4b3216e";
-        public static string IotHubConnectionString = "HostName=420-6P3.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=FHi9mLIpTlAZC3im336ASe43wO7e4Ydx+WrY4BcLe2k=";
-        public static string IotHubDeviceConnectionString = "HostName=420-6P3.azure-devices.net;DeviceId=Grass-Touchers;SharedAccessKey=8+Um5Wtl6vfV0wVfukg1wE3nh4fElnEXz1BaQFkEOqI=";
-        public static string IotHubDeviceId = "device-1-lab8";
-        public static string IotHubModuleId = "grass-touchers";
+        public static string EventHubConnectionString = "INSERT_VALID_NAME_HERE";
+        public static string EventHubName = "INSERT_VALID_NAME_HERE";
+        public static string IotHubConnectionString = "INSERT_VALID_NAME_HERE";
+        public static string IotHubDeviceConnectionString = "INSERT_VALID_NAME_HERE";
+        public static string IotHubDeviceId = "INSERT_VALID_NAME_HERE";
+        public static string IotHubModuleId = "INSERT_VALID_NAME_HERE";
     }
 }
